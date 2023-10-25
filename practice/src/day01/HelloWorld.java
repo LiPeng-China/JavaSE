809a4319c496fdc0e65676810f72be5f3fa0a69c
taiyi <pengli.cn@outlook.com>
2023-10-25 22:32:27 +0800

    finish:day01

---
+++ practice/src/day01/HelloWorld.java
@@ -0,0 +1,18 @@
+package day01;
+
+// public 类的修饰符
+// class 类的关键字
+// HelloWorld 类名
+// {} 类的范围
+public class HelloWorld {
+    // public 公共的，修饰符
+    // static 代表静态的，一个关键字
+    // void 代表没有返回类型
+    // main 代表程序入口
+    // String[] args 代表数组，程序参数
+
+    public static void main(String[] args) {
+        // 输出语句 输出 Hello World!
+        System.out.println("Hello World!");
+    }
+}
---
+++ task/src/day01/StudentInfo.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "sy";
+        String gender = "boy";
+        int age = 18;
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(age);
+    }
+}
