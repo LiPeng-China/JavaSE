b995c4e8705545e85559819b0d5e49aca82ce74d
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 08:24:31 +0800

    finish:day02_practice_DataTypeDemo

---
+++ practice/src/day02/DataTypeDemo.java
@@ -0,0 +1,42 @@
+package day02;
+
+/**
+ * @description: 基本数据类型Demo
+ * @author: taiyi
+ * @date: 2023-10-26 8:19
+ */
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        // byte 练习
+        byte b = 10;
+        System.out.println(b);
+
+        // short 练习
+        short s = 20;
+        System.out.println(s);
+
+        // int 练习
+        int i = 30;
+        System.out.println(i);
+
+        // long 练习
+        long l = 40L;
+        System.out.println(l);
+
+        // float 练习
+        float f = 3.14f;
+        System.out.println(f);
+
+        // double 练习
+        double d = 3.1415926d;
+        System.out.println(d);
+
+        // char 练习
+        char c = 'A';
+        System.out.println(c);
+
+        // boolean 练习
+        boolean bool = true;
+        System.out.println(bool);
+    }
+}
