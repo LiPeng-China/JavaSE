efb36c3d1850bcdeeee76f6bd33159a0b2b584e8
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 08:39:44 +0800

    finish:day02_practice_CharDemo

---
+++ practice/src/day02/CharDemo.java
@@ -0,0 +1,13 @@
+package day02;
+
+/**
+ * @description: char 字符码表
+ * @author: taiyi
+ * @date: 2023-10-26 8:37
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println(c+1);
+    }
+}
