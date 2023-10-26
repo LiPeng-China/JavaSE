160ba13e3e76e562d4cc9c5428a0417c4da188bb
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 11:25:49 +0800

    finish:day02_practice_LogicOperator

---
+++ practice/src/day03/LogicOperator.java
@@ -0,0 +1,23 @@
+package day03;
+
+/**
+ * @description: 逻辑运算符
+ * @author: taiyi
+ * @date: 2023-10-26 11:07
+ */
+public class LogicOperator {
+    public static void main(String[] args) {
+        System.out.println(true & false);
+
+        System.out.println(true | false);
+
+        System.out.println(!true);
+
+        System.out.println(true ^ false);
+
+        System.out.println(4 ^ -3);
+        System.out.println(Integer.toBinaryString(4));
+        System.out.println(Integer.toBinaryString(-3));
+        System.out.println(Integer.toBinaryString(4 ^ -3));
+    }
+}
