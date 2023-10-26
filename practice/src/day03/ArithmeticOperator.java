23804dd09f5e07c736b578b71c1009e642f0317f
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 10:48:17 +0800

    finish:day02_practice_ArithmeticOperator

---
+++ practice/src/day03/ArithmeticOperator.java
@@ -0,0 +1,19 @@
+package day03;
+
+/**
+ * @description: 算数运算符
+ * @author: taiyi
+ * @date: 2023-10-26 10:30
+ */
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int num = 10;
+        num = -num;
+        System.out.println(num);
+
+        int num1 = 5;
+        int num2 = 2;
+        int num3 = num1/num2;
+        System.out.println(num3);
+    }
+}
