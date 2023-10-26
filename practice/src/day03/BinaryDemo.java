78dbaa1bfa1a60e8ddd92b19523f48fff12cd15b
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 11:07:49 +0800

    finish:day02_practice_BinaryDemo

---
+++ practice/src/day03/BinaryDemo.java
@@ -0,0 +1,12 @@
+package day03;
+
+/**
+ * @description: 二进制数
+ * @author: taiyi
+ * @date: 2023-10-26 10:48
+ */
+public class BinaryDemo {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(-1));
+    }
+}
