4b3e6296177f0f9ff29a26cafa0a78b377887ccb
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 09:48:39 +0800

    finish:day02_practice_VariableDemo

---
+++ practice/src/day03/VariableDemo.java
@@ -0,0 +1,22 @@
+package day03;
+
+/**
+ * @description:
+ * @author: taiyi
+ * @date: 2023-10-26 9:43
+ */
+public class VariableDemo {
+    public static void main(String[] args) {
+        // 定义一个3.5 的变量
+        double d = 3.5;
+        // 重新给变量赋值
+        d = 3.6;
+        System.out.println(d);
+
+        // 定义变量 张三 的 名字
+        String name = "张三";
+        System.out.println(name);
+        name = "李四";
+        System.out.println(name);
+    }
+}
