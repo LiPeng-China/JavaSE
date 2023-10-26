8088bf7335ef137ffcf93432c55e16039713c8c3
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 15:30:33 +0800

    finish:day03 task03

---
+++ task/day03/src/org/taiyi/JudgeStudentAge.java
@@ -0,0 +1,14 @@
+package org.taiyi;
+
+/**
+ * @description: task03
+ * @author: taiyi
+ * @date: 2023-10-26 15:26
+ */
+public class JudgeStudentAge {
+    public static void main(String[] args) {
+        int mingAge = 18;
+        int gangAge = 17;
+        System.out.println(mingAge == gangAge ? true : false);
+    }
+}
