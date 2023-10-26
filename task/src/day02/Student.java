c481c1e4da726a0fe1b52b4c2b40d85d114e133b
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 09:11:28 +0800

    finish:day02_task_Student

---
+++ task/src/day02/Student.java
@@ -0,0 +1,29 @@
+package day02;
+
+/**
+ * @description: 这是一个学生类
+ * @author: taiyi
+ * @date: 2023-10-26 9:06
+ */
+public class Student {
+    public static void main(String[] args) {
+        //  学生姓名
+        String name = "张三";
+        System.out.println(name);
+        // 学生年龄
+        int age = 18;
+        System.out.println(age);
+        // 学生身高
+        int height = 180;
+        System.out.println(height);
+        // 学生性别
+        String gender = "男";
+        System.out.println(gender);
+        // 家庭住址
+        String address = "上海";
+        System.out.println(address);
+        // 体重
+        int weight = 60;
+        System.out.println(weight);
+    }
+}
