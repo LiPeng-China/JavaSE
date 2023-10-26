b104e1c84c911e6572bb42ed86fa934a5c6c5cae
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-26 15:09:04 +0800

    finish:day02_practice_ConditionOperator

---
+++ practice/src/day03/ConditionOperator.java
@@ -0,0 +1,18 @@
+package day03;
+
+/**
+ * @description: 条件运算符
+ * @author: taiyi
+ * @date: 2023-10-26 11:27
+ */
+public class ConditionOperator {
+    public static void main(String[] args) {
+        // 三元运算符 (三目运算符)
+        String res = false ? "你好" : "你不好";
+        System.out.println(res);
+
+        int age = 19;
+        String res1 = age >= 18? "可以上网": "拒绝上网";
+        System.out.println(res1);
+    }
+}
