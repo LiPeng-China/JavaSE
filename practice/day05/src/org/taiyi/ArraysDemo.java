e9152ace2fe77db4085189d3af03f69e60c9e2fe
LiPeng-China <lipeng_cn@foxmail.com>
2023-10-27 09:53:35 +0800

    finish:practice_day05_ArraysDemo

---
+++ practice/day05/src/org/taiyi/ArraysDemo.java
@@ -0,0 +1,17 @@
+package org.taiyi;
+
+import java.util.Arrays;
+
+/**
+ * @description: 数组工具类 Arrays
+ * @author: taiyi
+ * @date: 2023-10-27 8:22
+ */
+public class ArraysDemo {
+    public static void main(String[] args) {
+        int[] ages = {1, 3, 4, 6, 9};
+
+        String s = Arrays.toString(ages);
+        System.out.println(s);
+    }
+}
